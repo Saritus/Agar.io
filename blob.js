3428f9ff2b5d984ce666caa2b50a69019ec2f3ea
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 19:30:17 +0100

    Fix the eats function

--- blob.js
+++ blob.js
@@ -11,7 +11,8 @@ function Blob(x, y, r) {
   this.eats = function(other) {
     var d = p5.Vector.dist(this.pos, other.pos);
     if (d < this.r + other.r) {
-      this.r += other.r;
+      var sum = PI * this.r * this.r + PI * other.r * other.r;
+      this.r = sqrt(sum/PI);
       return true;
     }
     else {
