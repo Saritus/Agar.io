0ae8e805679b5a392e8ed61d5c7601181b938741
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 19:34:51 +0100

    Fix a camera problem

--- sketch.js
+++ sketch.js
@@ -3,16 +3,19 @@ var blobs = [];
 
 function setup() {
   createCanvas(500, 500);
-  blob = new Blob(width/2, height/2, 64);
+  blob = new Blob(0, 0, 64);
   for (var i = 0; i < 50; i++) {
-    blobs[i] = new Blob(random(-width, 2*width), random(-height, 2*height), 16);
+    blobs[i] = new Blob(random(-width, width), random(-height, height), 16);
   }
 }
 
 function draw() {
   background(0);
 
-  translate(width/2-blob.pos.x, height/2-blob.pos.y);
+  translate(width/2, height/2);
+  scale(64 / blob.r);
+  translate(-blob.pos.x, -blob.pos.y);
+
 
   for (var i = blobs.length-1; i >= 0; i--) {
     blobs[i].show();
