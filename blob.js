2fc867908900b7cea6ec06a726aa772b5abb6248
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-09 19:26:49 +0100

    Add eats function

--- blob.js
+++ blob.js
@@ -8,6 +8,17 @@ function Blob(x, y, r) {
     this.pos.add(vel);
   }
 
+  this.eats = function(other) {
+    var d = p5.Vector.dist(this.pos, other.pos);
+    if (d < this.r + other.r) {
+      this.r += other.r;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
   this.show = function() {
     fill(255);
     ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var blobs = [];
 function setup() {
   createCanvas(500, 500);
   blob = new Blob(width/2, height/2, 64);
-  for (var i = 0; i < 10; i++) {
-    blobs[i] = new Blob(random(width), random(height), 16);
+  for (var i = 0; i < 50; i++) {
+    blobs[i] = new Blob(random(-width, 2*width), random(-height, 2*height), 16);
   }
 }
 
@@ -13,9 +13,14 @@ function draw() {
   background(0);
 
   translate(width/2-blob.pos.x, height/2-blob.pos.y);
+
+  for (var i = blobs.length-1; i >= 0; i--) {
+    blobs[i].show();
+    if (blob.eats(blobs[i])) {
+      blobs.splice(i, 1);
+    }
+  }
+
   blob.show();
   blob.update();
-  for (var i = 0; i < blobs.length; i++) {
-    blobs[i].show();
-  }
 }
